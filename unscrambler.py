4942ce304566448f18edf0b76ab5c893a609c2d8
miguel <moogwol@msn.com>
2019-10-25 08:15:22 +0200

    added a function to import a list from a .txt file

--- unscrambler.py
+++ unscrambler.py
@@ -1,7 +1,13 @@
-
+def convert_to_list():
+    file = 'scrambled_words.txt'
+    scrambled_words_list = []
+    with open(file, 'r') as f:
+        for word in f:
+            word = word[:-1].lower()
+            scrambled_words_list.append(word)
+    return scrambled_words_list
 
 def dictionary():
-
     word_list = []
     file = 'sowpods.txt'
     with open(file, 'r') as f:
@@ -11,18 +17,18 @@ def dictionary():
     return word_list
 
 
-
-def unscramble(scrambled_list):
-
-    for scrambled_word in scrambled_list:
+def unscramble():
+    for scrambled_word in convert_to_list():
         sowpods = dictionary()
-        for word in scrambled_list:
+        for word in convert_to_list():
             for i in sowpods:
                 if sorted(word) == sorted(i):
-                    print(i)
+                    print(i, end=', ')
                     break
         break
-unscramble(['dnashciw'])
+
+
+unscramble()
 
 
 
